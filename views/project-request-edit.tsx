5c989689fd1e7b872c623f1755896fc02953678f
Taylor <>
2026-08-03 18:39:20 +0800

    fixed: bugs

--- views/project-request-edit.tsx
+++ views/project-request-edit.tsx
@@ -254,7 +254,7 @@ function recipientLabel(email: string) {
 }
 
 /* HQ recipients cc'd on every project-request email (sent from a system address). */
-const HQ_CC_RECIPIENTS = ['Jasline', 'Jenyn'];
+const HQ_CC_RECIPIENTS = ['Jasmine', 'Jeryn'];
 
 const TEMPLATE_FILENAME = 'DSTA_Project_Request_Template.xlsx';
 
